687218318ca296746e4fa12d7de3d00001c03c74
Kayla Miller <kayla@kaylamiller.dev>
2023-02-11 11:20:20 -0500

    added placeholder routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Backend' });
+router.post('/couponcode', (req, res, next) => {
+  res.end('will apply the coupon code to the order')
+});
+
+router.get('/menu', (req, res, next) => {
+  res.end('will get the menu');
+});
+
+router.get('/locations', (req, res, next) => {
+  res.end('will get restauramt locations');
+});
+
+router.get('/cart', (req, res, next) => {
+  res.end('will get cart');
+});
+
+router.post('/placeOrder', (req, res, next) => {
+  res.end('will Place an order');
+});
+
+router.get('/promoOrder', (req, res, next) => {
+  res.end('will get the info to order the promo pizza from the home page');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', (req, res, next) => {
+  res.end('will get login page');
+});
+
+router.post('/login', (req, res, next) => {
+  res.end('will login the user');
+})
+
+router.post('/signup', (req, res, next) => {
+  res.end('will signup the user');
+});
+
+router.post('/logout', (req, res, next) => {
+  res.end('will log out the user');
 });
 
 module.exports = router;
