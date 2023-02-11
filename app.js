4dc7c412bd5c5dce81638385a17748b654586448
Kayla Miller <kayla@kaylamiller.dev>
2023-02-11 11:26:07 -0500

    testing nodemodules stuff

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+console.log('test');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
