3589583f45a893adecd7481b8906c88751b89973
Clay Carney <102885927+TheClutchC@users.noreply.github.com>
2023-03-29 11:06:21 -0400

    initial fetch request setup FE to BE

--- routes/menu.js
+++ routes/menu.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/menuItems', () => {
-
+router.get('/menuItems', (req, res, next) => {
+  res.json("You've reached /menuItems");
 });
 
 module.exports = router;
