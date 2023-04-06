865a6356d91a860224ab8cbe2fc638b7770e514f
Kayla Miller <kayla.miller.km@outlook.com>
2023-04-06 12:03:23 -0400

    clean up menu model and rename from menus to menu

--- models/menu.js
+++ models/menu.js
@@ -2,15 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const menuSchema = new Schema({
-  // _id: mongoose.Schema.Types.ObjectId,
   title: {
     type: String,
     required: true
   },
- /*  menuSections: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'MenuSections'
-  }]  */
   menuSections: Array
 }, {collection: 'menu'});
 module.exports = mongoose.model("menu", menuSchema)
